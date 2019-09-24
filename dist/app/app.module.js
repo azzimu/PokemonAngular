ac5829b9dd23cad09f26d42c9ff8d2fbb5a5d015
azzi <azzimu@gmail.com>
2019-09-24 12:46:13 +0200

    Services ok Bug login

---
+++ dist/app/app.module.js
@@ -0,0 +1,36 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@angular/core");
+var platform_browser_1 = require("@angular/platform-browser");
+var http_1 = require("@angular/common/http");
+var angular_in_memory_web_api_1 = require("angular-in-memory-web-api");
+var app_component_1 = require("./app.component");
+var app_routing_module_1 = require("./app-routing.module");
+var page_not_found_component_1 = require("./page-not-found.component");
+var pokemon_module_1 = require("./pokemons/pokemon.module");
+var in_memory_data_service_1 = require("./in-memory-data.service");
+var forms_1 = require("@angular/forms");
+var login_component_1 = require("./login.component");
+var login_routing_module_1 = require("./login-routing.module");
+var AppModule = /** @class */ (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        core_1.NgModule({
+            imports: [platform_browser_1.BrowserModule,
+                http_1.HttpClientModule, forms_1.FormsModule,
+                angular_in_memory_web_api_1.HttpClientInMemoryWebApiModule.forRoot(in_memory_data_service_1.InMemoryDataService, { dataEncapsulation: false }), pokemon_module_1.PokemonsModule, login_routing_module_1.LoginRoutingModule, app_routing_module_1.AppRoutingModule],
+            declarations: [app_component_1.AppComponent, page_not_found_component_1.PageNotFoundComponent, login_component_1.LoginComponent],
+            bootstrap: [app_component_1.AppComponent]
+        })
+    ], AppModule);
+    return AppModule;
+}());
+exports.AppModule = AppModule;
+//# sourceMappingURL=app.module.js.map
---
+++ dist/app/pokemons/pokemons-routineg.module.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=pokemons-routineg.module.js.map
